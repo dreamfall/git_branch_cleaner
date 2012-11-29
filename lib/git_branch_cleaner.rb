59ce89a97722d282527dfb0b1a698a74668a8124
dreamfall <amarant.st@gmail.com>
2012-11-29 20:37:12 +0300

    add branch cleanuper

--- lib/git_branch_cleaner.rb
+++ lib/git_branch_cleaner.rb
@@ -1,5 +1,32 @@
 require "git_branch_cleaner/version"
+require "shellwords"
+require "highline/import"
 
 module GitBranchCleaner
-  # Your code goes here...
+  class Cleanuper
+    def initialize(force)
+      @force = force
+    end
+
+    def execute!
+      `git fetch`
+      branches = `git branch -r`.split("\n")
+
+      branches.reject! { |branch| `git branch --contains #{branch}` =~ /master/ }
+      branches.map! { |branch| [branch.split('/')[0].strip, branch.split('/')[1].strip] }
+
+      branches.each do |remote, branch_name|
+        confirmed = @force
+
+        unless confirmed
+          input = ask("Are you sure you want to delete branch #{branch_name} on remote #{remote}? (Y/n)")
+          confirmed = input =~ /[Yy]/ || input.empty?
+        end
+
+        if confirmed
+          `git push #{Shellwords.escape(remote)} :#{Shellwords.escape(branch_name)}`
+        end
+      end
+    end
+  end
 end
